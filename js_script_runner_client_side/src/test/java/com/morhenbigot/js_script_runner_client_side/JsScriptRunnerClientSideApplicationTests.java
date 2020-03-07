d1e489f502f3e65aca6525cc9da5de1ace0faa7c
Ruslan <Ruslan.Lomaka@Gmail.com>
2020-03-07 17:10:56 +0200

    Base architecture

---
+++ js_script_runner_client_side/src/test/java/com/morhenbigot/js_script_runner_client_side/JsScriptRunnerClientSideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.morhenbigot.js_script_runner_client_side;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsScriptRunnerClientSideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
